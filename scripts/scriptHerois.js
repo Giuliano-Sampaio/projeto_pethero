05189261a8863d8b2e3b8341ab572a1b12e810a4
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2023-08-26 09:34:03 -0300

    limpeza no codigo refente herois

--- scripts/scriptHerois.js
+++ scripts/scriptHerois.js
@@ -16,10 +16,6 @@ const herois = [
   {},
 ];
 
-/* function openModal() {
-  document.getElementById("myModal").style.display = "flex";
-} */
-
 function openModal(idHeroi) {
   const heroi = herois.find((item)=> {
     return item.id === Number(idHeroi);
@@ -49,42 +45,3 @@ function openModal(idHeroi) {
 function closeModal() {
   document.getElementById("myModal").style.display = "none";
 }
-
-/*
-  `Cuidadora ${heroi.nome}`
-  foto ${heroi.fotoModal}
-  heroi.frase
-  heroi.pix
-
-
-*/
-
-/* <!-- <section class="selected">
-        <h2>Cuidadora Ana Maria</h2>
-        <div class="card-selected">
-          <img
-            id="imgCardSelected"
-            src="../img/abrigo1000x500.jpeg"
-            alt="Foto Cuidadora Ana Paula"
-          />
-          <p>
-            “Minha vida é lutar pelos animais que são abandonados a propria
-            sorte,<br />
-            para mim eles são anjinhos que não falam a nossa lingua mas estão
-            nesse<br />
-            mundo apenas para nos dar amor. Se vc se identifica com essa
-            causa<br />
-            Junte-se a mim.”
-          </p>
-
-          <h3>Faça uma doação</h3>
-          <p>Pix CPF: 055.999.555-99</p>
-          <span>Ana Paula Moreira</span>
-          <span> - </span>
-          <span>Banco do Brasil</span>
-          <p>ou compareça ao local para serviço volutário</p>
-          <span>cidade: </span>
-          <span>Joinville/SC</span>
-          <p>endereço: Rua das Palmas 132 CEP 89230-670</p>
-        </div>
-      </section> --> */
