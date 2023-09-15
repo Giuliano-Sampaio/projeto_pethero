08ec368ea3b8d06ed8b56fa499f02c6d955c4dcd
Raimundo Néto <raimundormneto@outlook.com>
2023-09-15 17:31:09 -0300

    Ajustes de Layout
    Ajustando responsividade e layout

---
+++ scripts/scriptHome.js
@@ -0,0 +1,6 @@
+//Adicione o JavaScript para o menu responsivo aqui
+
+    function toggleMenu() {
+        const menu = document.querySelector('.navbar-menu');
+        menu.classList.toggle('active');
+    }
