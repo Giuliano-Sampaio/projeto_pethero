6a2bd479af2e18ddc9fe66b5a88c1f6e78f4f634
bianca <blas.bianca.sosa@gmail.com>
2023-09-27 18:14:42 -0300

    Nome e foto agora são inseridas pelo java

--- scripts/scriptCuidador.js
+++ scripts/scriptCuidador.js
@@ -1,8 +1,40 @@
 function obterParametroURL(nome) {
-    const urlSearchParams = new URLSearchParams(window.location.search);
+    const urlSearchParams = new URLSearchParams(window.location.search);//propriedade search procura o que vêm após "?"
     return urlSearchParams.get(nome);
 }
 
-// Obtenha o ID do usuário da URL
+let nome = document.getElementById("nome")
+let imgCuidador = document.getElementById("imgCuidador")
+
+// Obtendo o ID do usuário da URL
 const userID = obterParametroURL('userID');
 console.log(userID)
+
+let listaUsuarios = [ /*array de objetos, cada objeto contém as informações dos usuários*/
+        {
+            userIDcad:"001",
+            nome:'Rosana',
+            foto: '../img/hero01.jpg',
+            emailCad:'rosana@pethero.com.br',
+            telefone:'(47)99999-8888',
+            dataNasc:'12/12/1970',
+            descricao: 'Minha vida é lutar pelos animais que são abandonados a propria sorte, para mim eles são anjinhos que não falam a nossa lingua mas estão neste mundo apenas para nos dar amor. Se vc se identifica com essa causa Junte-se a mim',
+            pet1Nome: 'Bella',
+            pet1Foto: '../img/adoteUmPet/dog1.jpg',
+            
+        },
+
+        {
+            userIDcad:"002",
+            nome:'Ana',
+            foto: '../img/hero02.jpg'
+        }
+    ]
+
+    listaUsuarios.forEach((item) => { /*verificando cada item o array de objetos*/
+    if(userID == item.userIDcad){        
+        nome.innerHTML = item.nome
+        imgCuidador.src = item.foto       
+    }
+    
+})
