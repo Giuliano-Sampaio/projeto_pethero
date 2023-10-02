e6b73fd173db37d74224386d39fd16e2dae32fd6
bianca <blas.bianca.sosa@gmail.com>
2023-10-02 17:59:57 -0300

    direcionando para dashboard após login

--- scripts/scriptLogin.js
+++ scripts/scriptLogin.js
@@ -5,38 +5,44 @@ let button = document.getElementById ("button")
 let listaUsuarios = [ /*array de objetos, cada objeto contém as informações dos usuários*/
         {
             emailCad:'rosana@pethero.com.br',
-            senhaCad:'654321',
+            senhaCad:'123456',
             userIDcad:"001"
         },
 
         {
-            emailCad:'bianca@hotmail.com',
+            emailCad:'ana@mail.com',
             senhaCad:'123456',
             userIDcad:"002"
         }
     ]
-
-
-
-
 
 formulario.addEventListener("submit", function (event) {
     event.preventDefault(); // Evita o envio padrão do formulário
 
     let inputEmail = document.getElementById("imputEmail")
     let inputSenha = document.getElementById("imputSenha")
-    
+    let userID
+    let autentic = false
+        
     listaUsuarios.forEach((item) => { /*verificando cada item o array de objetos*/
+    
         if(inputEmail.value == item.emailCad && inputSenha.value == item.senhaCad){
-            const userID = item.userIDcad
-            window.location.href = `contaCuidador.html?userID=${userID}`;
-            
+            userID = item.userIDcad
+            autentic = true            
         }
-        else{
-            alert("usuário ou senha incorretos")
-        }
+        
     })
+
+    if (autentic == true) {        
+        window.location.href = `../pages/dashboard/dashboardUser.html?userID=${userID}`;
+        return;
+    }
+    else{
+        alert("Usuário ou senha incorretos")        
+    }
+
   });
+
 
     
 
