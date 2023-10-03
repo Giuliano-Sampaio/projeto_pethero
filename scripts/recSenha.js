03a81025c234645a498f4731eb584d8017ceca7e
Raimundo Néto <raimundormneto@outlook.com>
2023-10-03 14:08:21 -0300

    Chore

--- scripts/recSenha.js
+++ scripts/recSenha.js
@@ -9,12 +9,14 @@ function confirmarEmail(email) {
             const messageDiv = document.getElementById("message");
 
             if (confirmarEmail(emailDigitado)) {
-                messageDiv.innerHTML = "E-mail de recuperação válido. Enviando instruções de recuperação.";
-                messageDiv.style.color = "green";
+                alert('E-mail de recuperação válido. Enviando instruções de recuperação.')
+                // messageDiv.innerHTML = "E-mail de recuperação válido. Enviando instruções de recuperação.";
+                // messageDiv.style.color = "green";
                 return true; 
             } else {
-                messageDiv.innerHTML = "Endereço de e-mail de recuperação inválido. Por favor, verifique o e-mail digitado.";
-                messageDiv.style.color = "red";
+                alert('Endereço de e-mail de recuperação inválido. Por favor, verifique o e-mail digitado.')
+                // messageDiv.innerHTML = "Endereço de e-mail de recuperação inválido. Por favor, verifique o e-mail digitado.";
+                // messageDiv.style.color = "red";
                 return false; 
             }
         }
