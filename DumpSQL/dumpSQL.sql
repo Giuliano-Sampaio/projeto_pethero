9226d88744e7c45913f2310ce1729ed29a967b7d
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2023-10-08 17:01:42 -0300

    populando tabela cuidadores e animaisCadastrados

--- DumpSQL/dumpSQL.sql
+++ DumpSQL/dumpSQL.sql
@@ -126,3 +126,38 @@ WHERE comentario_id = 1;
 
 /* Excluir um registro de Comentarios (Delete): */
 DELETE FROM Comentarios WHERE comentario_id = 1;
+
+
+/* dados para popular o banco de dados */
+INSERT INTO Cuidadores
+(nome, email, telefone, dataNascimento, imagem, endereco, cidade, estado, pais)
+VALUES
+('João Silva', 'joao@example.com', '1234567890', '1985-05-15', 'imagem_url_1', 'Avenida Principal', 'São Paulo', 'SP', 'Brasil'),
+('Maria Souza', 'maria@example.com', '9876543210', '1990-02-22', 'imagem_url_2', 'Rua das Flores', 'Rio de Janeiro', 'RJ', 'Brasil'),
+('Pedro Santos', 'pedro@example.com', '5555555555', '1988-07-10', 'imagem_url_3', 'Boulevard Avenue', 'Belo Horizonte', 'MG', 'Brasil'),
+('Ana Oliveira', 'ana@example.com', '7777777777', '1982-11-30', 'imagem_url_4', 'Rua das Palmeiras', 'Salvador', 'BA', 'Brasil'),
+('Carlos Pereira', 'carlos@example.com', '1111111111', '1995-04-18', 'imagem_url_5', 'Avenida da Praia', 'Fortaleza', 'CE', 'Brasil')
+;
+
+INSERT INTO AnimaisCadastrados (nome, raca, idade, imagem)
+VALUES
+('Bella', 'Golden Retriever', '3 anos', 'imagem_url_1'),
+('Max', 'Labrador', '4 anos', 'imagem_url_2'),
+('Luna', 'Bulldog Francês', '2 anos', 'imagem_url_3'),
+('Charlie', 'Beagle', '1 ano', 'imagem_url_4'),
+('Lucy', 'Poodle', '5 anos', 'imagem_url_5'),
+('Cooper', 'Rottweiler', '3 anos', 'imagem_url_6'),
+('Mia', 'Dachshund', '2 anos', 'imagem_url_7'),
+('Rex', 'Husky Siberiano', '4 anos', 'imagem_url_8'),
+('Rock', 'Chihuahua', '1 ano', 'imagem_url_9'),
+('Bailey', 'Pug', '3 anos', 'imagem_url_10'),
+('Lily', 'Dálmata', '2 anos', 'imagem_url_11'),
+('Leo', 'Border Collie', '4 anos', 'imagem_url_12'),
+('Zoe', 'Bichon Frisé', '5 anos', 'imagem_url_13'),
+('Risco', 'Doberman', '3 anos', 'imagem_url_14'),
+('Toba', 'Shih Tzu', '2 anos', 'imagem_url_15'),
+('Flash', 'Boxer', '1 ano', 'imagem_url_16'),
+('Oscar', 'Schnauzer', '3 anos', 'imagem_url_17'),
+('Coco', 'Bulldog Inglês', '4 anos', 'imagem_url_18'),
+('Lola', 'Akita', '2 anos', 'imagem_url_19'),
+('Mito', 'Yorkshire Terrier', '5 anos', 'imagem_url_20');
